319f1f30756a66a6d00165fb34e92e639208e1d1
jayant thorat <jayantthorat1999@gmail.com>
2024-10-10 01:07:19 +0530

    modified single imageDetails

--- image gallary/Components/SingleImageDetails/SingleImageDetails.jsx
+++ image gallary/Components/SingleImageDetails/SingleImageDetails.jsx
@@ -33,12 +33,13 @@ function SingleImageDetails() {
   });
 
   return (
-    <div className="col-10 m-auto my-5 text-white single-bg py-5 d-flex">
-      <div className=" col-6 ">
+    <div className="col-10 m-auto my-5 text-white single-bg py-5 d-flex justify-content-center align-items-center">
+      <div className=" col-5 ">
         <img src={imageState.image} alt="" className="single-images " />
       </div>
-      <div className="col-6 p-4">
+      <div className="col-7 d-flex justify-content-center align-items-start p-3 flex-column">
         <h2>{imageState.title}</h2>
+
         <br />
         <p className="h4">{imageState.description}</p>
       </div>
