cf9fc4dc6b5ce001344e7e9fd253c304e305ec13
jayant thorat <jayantthorat1999@gmail.com>
2024-10-09 20:06:34 +0530

    tested and fetched the image gallary api

---
+++ image gallary/Components/singleImageDetails/SingleImage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './SingleImage.css'
+
+function SingleImage({url}) {
+    return (
+    
+        <div className=''>
+
+        <img src={url} alt="" />
+        
+        </div>
+        
+
+    )
+}
+
+export default SingleImage
--- image gallary/src/App.jsx
+++ image gallary/src/App.jsx
@@ -1,8 +1,54 @@
 
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import './App.css'
+import SingleImage from '../Components/singleImageDetails/SingleImage'
 
 function App() {
 
+const [imageListState,setImageListState] = useState({
+      imageListUrl:'https://api.slingacademy.com/v1/sample-data/photos?offset=5&limit=20',
+      images:[]
+
+})
+
+async function ImgaeDownload(){
+    const response = await axios.get(imageListState.imageListUrl)
+    
+    const photoList = response.data.photos 
+    console.log(photoList);
+    
+   const list =  photoList.map(i => i.url)
+
+   const pictures = await axios.all(list)
+  setImageListState({...imageListState,images:pictures})
+   
+
+    
+    
+    
+}
+
+useEffect(()=>{
+  ImgaeDownload()
+})
+
+
+return(
+<>
+
+
+{
+  imageListState.images.map((url)=> 
+    <SingleImage
+    url={url}
+    />
+  )
+}
+
+
+</>
+)
 }
 
 export default App
